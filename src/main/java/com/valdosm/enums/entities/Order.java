f9c379781fd3c77e6cd7f14d73ee7471d87343ee
Moreira <valdosm.moreira@gmail.com>
2023-05-19 04:33:55 -0300

    jakarta version

--- src/main/java/com/valdosm/enums/entities/Order.java
+++ src/main/java/com/valdosm/enums/entities/Order.java
@@ -1,11 +1,11 @@
 package com.valdosm.enums.entities;
 
 import java.time.Instant;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
